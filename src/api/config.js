56be2bda83fefbe353ac908062f8a7478055379d
xuanqiYan <173109209032163.com>
2019-07-03 10:49:56 +0800

    qq-music

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export const commonParams = {
+  _: 1562068655709,
+  g_tk: 5381,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
